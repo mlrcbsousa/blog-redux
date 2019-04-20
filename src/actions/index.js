6b01137aa2c14fe35a626b5084857dd30c2ca6e9
Manuel Sousa <mlrcbsousa@gmail.com>
2019-04-20 15:56:22 +0200

    redux blog with router

---
+++ src/actions/index.js
@@ -0,0 +1,39 @@
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_POST = 'FETCH_POST';
+export const POST_CREATED = 'POST_CREATED';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
+const API_KEY = 'WAGON-BLOG';
+
+export function fetchPosts() {
+  const promise = fetch(`${ROOT_URL}?key=${API_KEY}`)
+    .then(r => r.json());
+
+  return {
+    type: FETCH_POSTS,
+    payload: promise
+  };
+}
+
+export function fetchPost(id) {
+  const promise = fetch(`${ROOT_URL}/${id}?key=${API_KEY}`)
+    .then(response => response.json());
+  return {
+    type: FETCH_POST,
+    payload: promise
+  };
+}
+
+export function createPost(body, callback) {
+  const request = fetch(`${ROOT_URL}?key=${API_KEY}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  }).then(response => response.json())
+    .then(callback);
+
+  return {
+    type: POST_CREATED,
+    payload: request
+  };
+}
